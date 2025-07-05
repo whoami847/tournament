33d8905ba93aa736175ca8ec86c0b939bbfaae70
Who Am i <whoami999901@gmail.com>
2025-07-05 10:10:05 +0000

    Akon amake full guidelines deo full details soho

---
+++ capacitor.config.ts
@@ -0,0 +1,13 @@
+
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.esportshq.app',
+  appName: 'Esports HQ',
+  webDir: 'out',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;

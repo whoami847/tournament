70b24e945d20006230a67ec0ffdb3fd9a5b345a1
Who Am i <whoami999901@gmail.com>
2025-07-06 12:21:05 +0000

    Ami amar webapp ta versel a deploy korte chi kintu onek error asche Akon

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -10,7 +9,6 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
